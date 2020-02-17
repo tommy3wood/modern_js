4384bbd613c0ddcaf6d9f6ca631ad6246f902a34
tommy3wood <thomaswcurran1014@gmail.com>
2020-02-17 10:49:57 -0600

    added bundle and webpack.config.js

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log("Farts push the poop out");
+var moment = require('moment');
+
+var math = require('mathjs');
+
+console.log("Poop pushes the farts out");
+
+var datetime = moment().startOf('month').fromNow();
+console.log(datetime);
+
+console.log(math.sqrt(-16));
+console.log("How do batteries work?")
+
+
+class Person{
+  constructor(height, weight, limbs, parts){
+    this.height = height;
+    this.weight = weight;
+    this.limbs = limbs;
+    this.parts = parts;
+  }
+  getInfo(){
+    return `The ${this.parts} is ${this.height} feet tall, has ${this.limbs} extremities, and weights ${this.weight} pounds` ;
+  }
+}
+
+var jim = new Person(6, 220, 4, "boy")
+console.log(jim);
+console.log(jim.getInfo());
